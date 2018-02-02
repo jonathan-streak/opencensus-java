0616d7f029440d2720f8ac8a07477f87b0363eb9
Jonathan Dobbie <jonathan@streak.com>
2018-02-01 16:45:58 -0800

    Create Daemons using background threads on GAE

--- impl_core/src/main/java/io/opencensus/implcore/internal/DaemonThreadFactory.java
+++ impl_core/src/main/java/io/opencensus/implcore/internal/DaemonThreadFactory.java
@@ -16,21 +16,48 @@
 
 package io.opencensus.implcore.internal;
 
-import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.base.Throwables;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /** A {@link ThreadFactory} implementation that starts all {@link Thread} as daemons. */
 public final class DaemonThreadFactory implements ThreadFactory {
+  public static final boolean IS_APPENGINE =
+      System.getProperty("com.google.appengine.runtime.environment") != null;
   // AppEngine runtimes have constraints on threading and socket handling
   // that need to be accommodated.
   public static final boolean IS_RESTRICTED_APPENGINE =
-      System.getProperty("com.google.appengine.runtime.environment") != null
-          && "1.7".equals(System.getProperty("java.specification.version"));
+      IS_APPENGINE && "1.7".equals(System.getProperty("java.specification.version"));
   private static final String DELIMITER = "-";
-  private static final ThreadFactory threadFactory = MoreExecutors.platformThreadFactory();
+  private static final ThreadFactory threadFactory = getThreadFactory();
   private final AtomicInteger threadIdGen = new AtomicInteger();
   private final String threadPrefix;
+
+  private static ThreadFactory getThreadFactory() {
+    if (!IS_APPENGINE) {
+      return Executors.defaultThreadFactory();
+    }
+    try {
+      String threadMethod =
+          "1.7".equals(System.getProperty("java.specification.version"))
+              ? "currentRequestThreadFactory"
+              : "backgroundThreadFactory";
+      return (ThreadFactory)
+          Class.forName("com.google.appengine.api.ThreadManager")
+              .getMethod(threadMethod)
+              .invoke(null);
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("Couldn't invoke ThreadManager.backgroundThreadFactory", e);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Couldn't invoke ThreadManager.backgroundThreadFactory", e);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException("Couldn't invoke ThreadManager.backgroundThreadFactory", e);
+    } catch (InvocationTargetException e) {
+      throw Throwables.propagate(e.getCause());
+    }
+  }
 
   /**
    * Constructs a new {@code DaemonThreadFactory}.
